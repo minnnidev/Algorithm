3df09587a6ddcf5ad292fe8047863306a74b20f9
Mini <minnnidev@gmail.com>
2022-11-08 11:43:12 +0900

    백준 10998

---
+++ BOJ-ByStep/BOJ-ByStep/10998.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  BOJ-ByStep
+//
+//  Created by 김민 on 2022/11/08.
+//
+
+let line = readLine()!
+let lineArr = line.split(separator: " ")
+
+let a = Int(lineArr[0])!
+let b = Int(lineArr[1])!
+
+print(a*b)
