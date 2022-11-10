afeae4a560ef02238a090685b6dc4b74ee160b6d
Mini <minnnidev@gmail.com>
2022-11-10 23:52:16 +0900

    백준 25083

---
+++ BOJ-ByStep/BOJ-ByStep/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  BOJ-ByStep
+//
+//  Created by 김민 on 2022/11/10.
+//
+//백준 25083
+
+let sprout = #"""
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+"""#
+
+print(sprout)
