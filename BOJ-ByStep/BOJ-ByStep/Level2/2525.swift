0bd5ce277266789ca6f42ae3a821789b5083b520
Mini <minnnidev@gmail.com>
2022-11-11 16:44:55 +0900

    백준 2525

---
+++ BOJ-ByStep/BOJ-ByStep/Level2/2525.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  BOJ-ByStep
+//
+//  Created by 김민 on 2022/11/11.
+//
+//백준 2525
+
+let time = readLine()!.split(separator: " ").map { Int($0)! }
+let C = Int(readLine()!)!
+
+let A = time[0]
+let B = time[1]
+
+let tmpHour = (B + C) / 60
+let tmpMinute = (B + C) % 60
+
+A+tmpHour >= 24 ? print(A+tmpHour-24, terminator: " ") : print(A+tmpHour, terminator: " ")
+B+C >= 60 ? print((B + C) % 60) : print(B+C)
