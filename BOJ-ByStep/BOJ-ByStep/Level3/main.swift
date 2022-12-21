b53bf8c404b7163190b12b08097a8ffeb13286fc
Mini <minnnidev@gmail.com>
2022-12-21 13:36:50 +0900

    백준 2739

---
+++ BOJ-ByStep/BOJ-ByStep/Level3/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  BOJ-ByStep
+//
+//  Created by 김민 on 2022/12/21.
+//
+// 백준 2739
+
+import Foundation
+
+let num = Int(readLine()!)!
+
+for i in 1...9 {
+    print("\(num) * \(i) = \(num*i)")
+}
