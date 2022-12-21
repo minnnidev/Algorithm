96e711da0ec077a3619683cb856e7fa886b2ccd3
Mini <minnnidev@gmail.com>
2022-12-21 13:51:54 +0900

    백준 8393

---
+++ BOJ-ByStep/BOJ-ByStep/Level3/8393.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  BOJ-ByStep
+//
+//  Created by 김민 on 2022/12/21.
+//
+//백준 8393
+
+let num = Int(readLine()!)!
+var sum = 0
+
+for i in 1...num {
+    sum += i
+}
+
+print(sum)
